beb9fe4d6e2e3ed9df3d1d5f3f88947d01efc8a2
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-10-01 21:07:46 +0200

    Better formatting

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
 use anyhow::*;
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use colored::Colorize;
+use human_format::*;
 use serde_json::Value;
 use thousands::Separable;
 
-const WIDTH: usize = 50;
+#[derive(Copy, Clone, Debug, ValueEnum)]
+enum Count {
+    Bytes,
+    Children,
+}
+impl Count {
+    fn format(&self, x: usize) -> String {
+        match self {
+            Count::Bytes => Formatter::new()
+                .with_scales(Scales::Binary())
+                .with_suffix("B")
+                .format(x as f64),
+            Count::Children => Formatter::new().with_scales(Scales::SI()).format(x as f64),
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
     json_file: String,
 
     #[arg(short, long, default_value_t = 0.0)]
     threshold: f32,
+
+    #[arg(short, long, value_enum, default_value_t = Count::Bytes)]
+    count: Count,
 }
 
 #[derive(Debug, Clone)]
 struct Node {
     tag: Option<String>,
-    size: usize,
+    len: usize,
+    size_b: usize,
+    size_c: usize,
     key_size: usize,
     children: Option<Vec<Node>>,
 }
@@ -36,12 +56,10 @@ impl Node {
                     .map(|c| Node::from_json(c, 0, String::new()))
                     .collect::<Vec<_>>();
                 Node {
-                    tag: Some(format!(
-                        "{} (×{})",
-                        tag,
-                        children.len().separate_with_commas()
-                    )),
-                    size: children.iter().map(|c| c.size).sum::<usize>(),
+                    tag: Some(tag),
+                    len: children.len(),
+                    size_b: children.iter().map(|c| c.size_b).sum::<usize>(),
+                    size_c: children.len() + children.iter().map(|c| c.size_c).sum::<usize>(),
                     key_size: children.iter().map(|c| c.key_size).sum::<usize>(),
                     children: None,
                 }
@@ -53,7 +71,9 @@ impl Node {
                     .collect::<Vec<_>>();
                 Node {
                     tag: Some(tag),
-                    size: children.iter().map(|c| c.size).sum::<usize>(),
+                    len: 0,
+                    size_b: children.iter().map(|c| c.size_b).sum::<usize>(),
+                    size_c: children.len() + children.iter().map(|c| c.size_c).sum::<usize>(),
                     key_size: _children.keys().map(|k| k.len()).sum::<usize>(),
                     children: Some(children),
                 }
@@ -61,44 +81,67 @@ impl Node {
         }
     }
 
+    fn size(&self, count: Count) -> usize {
+        match count {
+            Count::Bytes => self.size_b,
+            Count::Children => self.size_c,
+        }
+    }
+
     fn leaf(key_size: usize, size: usize, tag: String) -> Node {
         Node {
             tag: if tag.is_empty() { None } else { Some(tag) },
-            size,
+            len: 0,
+            size_b: size,
+            size_c: 0,
             key_size,
             children: None,
         }
     }
 
-    fn render(&self, total_size: usize, depth: usize, threshold: f32, pad_tag: usize) {
-        let rel_size = self.size as f32 / total_size as f32;
+    fn render(&self, total_size: usize, depth: usize, threshold: f32, count: Count, width: usize) {
+        // 11 + 6 + 2 = 19 chars required for numbers
+        // -> (WIDTH - 19)×2/3 for tagline
+        // -> (WIDTH - 19)×1/3 for bar
+        let w_tagline = ((width - 19) * 2) / 3;
+        let w_bar = width - 19 - w_tagline - 2;
+
+        let rel_size = self.size(count) as f32 / total_size as f32;
         if rel_size < threshold {
             return;
         }
 
         let indent = " ".repeat(2 * depth);
-        let header = format!(
-            "{}{:0width$} {:.2}%",
+        let cardinality = if self.len > 0 {
+            format!("[{}] ", self.len.to_string().separate_with_commas())
+        } else {
+            String::new()
+        };
+        let mut id = format!(
+            "{}{}{}",
             indent,
-            self.tag.as_ref().cloned().unwrap_or_default(),
+            cardinality,
+            &self.tag.clone().unwrap_or(String::new())
+        );
+        if id.len() > w_tagline {
+            id = format!("{}…", id.chars().take(w_tagline - 2).collect::<String>());
+        }
+
+        let header = format!(
+            "{:0w_tagline$} {:>6.2}% {:>11}",
+            id,
             100. * rel_size,
-            width = pad_tag,
+            format!("({})", count.format(self.size(count))),
+            w_tagline = w_tagline,
         );
         println!(
-            "{:60}{}",
+            "{:55} {}",
             header.truecolor(100 + (155 as f32 * rel_size) as u8, 100, 100),
-            "#".repeat((rel_size * WIDTH as f32) as usize)
+            "▒".repeat((rel_size * w_bar as f32) as usize)
         );
         if let Some(children) = &self.children {
-            let pad_tag = children
-                .iter()
-                .filter(|c| c.size as f32 / total_size as f32 >= threshold)
-                .map(|c| c.tag.as_ref().map(|x| x.len()).unwrap_or(0))
-                .max()
-                .unwrap_or(0);
-
             for child in children {
-                child.render(total_size, depth + 1, threshold, pad_tag);
+                child.render(total_size, depth + 1, threshold, count, width);
             }
         }
     }
@@ -116,8 +159,19 @@ fn main() -> Result<()> {
         "Root".to_owned(),
     );
 
-    // dbg!(root);
-    root.render(root.size, 0, args.threshold / 100., 0);
+    let width = if let Some((w, _)) = term_size::dimensions() {
+        w
+    } else {
+        100
+    };
+
+    root.render(
+        root.size(args.count),
+        0,
+        args.threshold / 100.,
+        args.count,
+        width,
+    );
 
     Ok(())
 }
